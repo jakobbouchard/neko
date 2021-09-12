b5889122a37c572a05b574439aedfd1afc8d660b
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2021-09-12 17:18:03 -0400

    Custom headers are cool, but I don't really need them

--- functions.php
+++ functions.php
@@ -126,11 +126,6 @@ function neko_scripts() {
 add_action( 'wp_enqueue_scripts', 'neko_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
