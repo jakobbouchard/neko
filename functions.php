ab9542353df5483973d85592978a9426a30a5752
Jakob Bouchard <jakob@jakobbouchard.dev>
2021-09-13 22:58:19 -0400

    a bit of cleanup

--- functions.php
+++ functions.php
@@ -130,11 +130,6 @@ add_action( 'wp_enqueue_scripts', 'neko_scripts' );
 require get_template_directory() . '/inc/template-tags.php';
 
 /**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
