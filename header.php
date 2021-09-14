b9ba50b6aa888a93ecb415038ec3822be3bfa61a
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2021-09-13 22:58:19 -0400

    a bit of cleanup

--- header.php
+++ header.php
@@ -16,14 +16,16 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'neko' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'neko' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
@@ -38,15 +40,11 @@
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
-			$neko_description = get_bloginfo( 'description', 'display' );
-			if ( $neko_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $neko_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'neko' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'neko' ); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
@@ -56,6 +54,7 @@
 			);
 			?>
 		</nav><!-- #site-navigation -->
+
+		<?php get_search_form(); ?>
 	</header><!-- #masthead -->
 
-	<?php get_search_form(); ?>
